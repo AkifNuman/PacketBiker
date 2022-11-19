d468d37a23555ed50df0a573e859851e720d7720
AkifNuman <67601379+AkifNuman@users.noreply.github.com>
2022-11-19 20:31:48 +0300

    Submitted TestPacketBiker
    Git Version 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# PacketBiker
+
+Developed with Unreal Engine 4
---
+++ Source/PacketBiker/PacketBiker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PacketBiker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PacketBiker, "PacketBiker" );
